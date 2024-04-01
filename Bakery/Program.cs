ca7298b78c3f051977f5bf29fd0f13d2042f33db
Samantha Callie <Hoomee90@gmail.com>
2024-04-01 15:31:12 -0700

    add product ordering

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.Design;
 using Bakery.Models;
 
 namespace Bakery
@@ -18,31 +19,84 @@ namespace Bakery
 			Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
 			Console.WriteLine("What would you like to order?");
 			MakeOrder();
-			
-			static void MakeOrder()
+		}
+		
+		static void MakeOrder()
+		{
+			Console.WriteLine("Bread is $5 a loaf");
+			Console.WriteLine("But there's a special deal: Buy 2 loaves, get 1 free");
+			Console.WriteLine("Each pastry is $2");
+			Console.WriteLine("But there's ANOTHER special deal: Buy 3 pastries, get 1 free");
+			Console.WriteLine("Type 'bread' or 'pastry' to chose what to order");
+			string userResponse = Console.ReadLine().ToLower();
+			if (userResponse != "bread" && userResponse != "pastry")
 			{
-				Console.WriteLine("Bread is $5 a loaf");
-				Console.WriteLine("But there's a special deal: Buy 2 loaves, get 1 free");
-				Console.WriteLine("Each pastry is $2");
-				Console.WriteLine("But there's ANOTHER special deal: Buy 3 pastries, get 1 free");
-				Console.WriteLine("Type 'bread' or 'pastry' to chose what to order");
+				Console.WriteLine("I'm sorry. We don't understand");
+				Console.WriteLine("Please try again");
+				MakeOrder();
+			}
+			else
+			{
+				ChooseAmount(userResponse);
+			}
+		}
+			
+		static void ChooseAmount(string orderType)
+		{
+			Console.WriteLine("How many orders of {0} would you like?", orderType);
+			string amount = Console.ReadLine();
+			if (int.TryParse(amount, out int number))
+			{
+				BakeryItem order = orderType == "bread" ? new Bread(number) : new Pastry(number);
+				Console.WriteLine("That amount will cost ${0}", order.FindTotalCost());
+				Console.WriteLine("Would you like to add our other product to your order?");
+				Console.WriteLine("'y' for yes, anything else to move to checkout");
 				string userResponse = Console.ReadLine().ToLower();
-				if (userResponse != "bread" && userResponse != "pastry")
+				if (userResponse == "y")
 				{
-					Console.WriteLine("I'm sorry. We don't understand");
-					Console.WriteLine("Please try again");
-					MakeOrder();
+					if (orderType == "bread")
+					{
+						ChooseAmount("pastry", order);
+					}
+					else
+					{
+						ChooseAmount("bread", order);
+					}
 				}
 				else
 				{
-					ChooseAmount(userResponse);
-				}
-				
-				static void ChooseAmount(string orderType)
-				{
-
+					Checkout();
 				}
 			}
+			else
+			{
+				Console.WriteLine("I'm sorry. We can't provide that amount of {0}", orderType);
+				Console.WriteLine("Please try again");
+				ChooseAmount(orderType);
+			}
+		}
+		
+		static void ChooseAmount(string orderType, BakeryItem previousOrder)
+		{
+			Console.WriteLine("How many orders of {0} would you like?", orderType);
+			string amount = Console.ReadLine();
+			if (int.TryParse(amount, out int number))
+			{
+				BakeryItem order = orderType == "bread" ? new Bread(number) : new Pastry(number);
+				Console.WriteLine("That order will add ${0} to your total", order.FindTotalCost());
+				Checkout();
+			}
+			else
+			{
+				Console.WriteLine("I'm sorry. We can't provide that amount of {0}", orderType);
+				Console.WriteLine("Please try again");
+				ChooseAmount(orderType, previousOrder);
+			}
+		}
+		
+		static void Checkout()
+		{
+			
 		}
 	}
 }
